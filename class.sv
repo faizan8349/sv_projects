3ddac1a4e0d2b685097a672ad5777f79b6fd53f0
faizan8349 <faizanahmed0922@gmail.com>
2026-08-11 06:50:17 +0000

    class code

---
+++ class.sv
@@ -0,0 +1,150 @@
+// // Code your design here
+// class a;
+  
+//   rand int bus;
+// endclass 
+
+// module top ;
+
+//   initial 
+//     begin 
+      
+//   a a1[];
+  
+//   a b;
+  
+
+      
+//       a1=new[10];
+      
+//       foreach (a1[i])
+//         begin 
+          
+//                 b=new();
+      
+//       b.randomize();
+//           a1[i]=b;
+          
+//           $display("%d",i,a1[i]);
+//         end 
+      
+//     end 
+  
+// endmodule 
+
+// class a;
+  
+//   rand int c;
+  
+// endclass 
+
+// module m;
+  
+//   initial 
+//     begin 
+      
+//       a a1[$];
+//       a b;
+     
+// //       a1=new[15];
+      
+//        b=new();
+         
+//       foreach(a1[i])
+        
+//         begin 
+          
+          
+//           b.randomize;                        //if we use randomize and object creation outside the foreach loop then only same value will come 
+          
+//           a1.pushback(b);
+          
+//           $display("%d",i,a1[i]);
+          
+//         end 
+//     end 
+  
+//       endmodule 
+
+// //  ---------------------------------------------------------------------------------------
+// class a;
+//   int aa=10;
+// endclass 
+
+// class b;
+//   int bb=20;
+// endclass 
+
+// class c;
+//   int cc=30;
+// endclass 
+
+// class a1 extends a;
+//   int a11;
+// endclass 
+
+// class b1 extends b;
+//   int b11;
+// endclass 
+
+// class c1 extends c;
+//   int c11;
+// endclass 
+
+// class base_class #(type a_1 = a, b_1 = b ,c_1 = c);
+  
+//  a_1 a_11;
+//  b_1 b_11;
+//   c_1 c_11;
+//   function new();
+    
+//     a_11=new();
+//     b_11=new();
+//     c_11=new();
+  
+//   endfunction 
+    
+//   function void f1();
+    
+//     $display("%d,%d,%d",a_11.aa,b_11.bb,c_11.cc);
+  
+//     endfunction 
+  
+// endclass
+  
+//   class my_class extends base_class #( a1 ,b1 ,c1);
+    
+//     function new();
+      
+//     a_11.a11=40;
+//     b_11.b11=50;
+//     c_11.c11=60;
+    
+//     endfunction
+    
+//     function void f1();
+      
+//       super.f1();
+      
+//       $display("%d,%d,%d",a_11.a11,b_11.b11,c_11.c11);
+      
+//     endfunction 
+    
+// endclass
+  
+//   module m();
+    
+//     my_class my_1;
+    
+//     initial 
+//       begin 
+        
+//         my_1 =new();
+       
+//         my_1.f1();
+        
+//       end 
+    
+//   endmodule 
+        
+//   ----------------------------------------------------------------------------------
